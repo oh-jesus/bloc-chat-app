bdf1891405a2b857cd59eafcb1518cb694c9daa9
Jesus A Martinez <martinez18.rmhs@gmail.com>
2018-05-21 19:22:04 -0700

    7:20-monday

--- src/components/User.js
+++ src/components/User.js
@@ -5,19 +5,15 @@ class User extends Component {
   constructor(props) {
     super(props);
 
-     this.signInWithPopup = this.signInWithPopup.bind(this);
+     this.signIn = this.signIn.bind(this);
      this.signOut = this.signOut.bind(this);
-
      this.userRef = this.props.firebase.database().ref('user');
   }
 
 
-  signInWithPopup() {
+  signIn() {
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopUp( provider );
-  }
-  signOut() {
-    this.props.firebase.auth().signOut();
+    this.props.firebase.auth().signInWithPopup( provider );
   }
 
   componentDidMount() {
@@ -26,13 +22,22 @@ class User extends Component {
     });
   }
 
+  signOut() {
+    this.props.firebase.auth().signOut();
+  }
+
   render() {
+    let userName = "Guest";
+if (this.props.user) {
+  userName = this.props.user.displayName;
+}
+console.log(this.props.user);
     return(
       <section>
-  			  <button onClick = {this.signInWithPopup}>sign in
+        <span>{userName}
+        </span>
+  			  <button onClick = {this.signIn}>sign in
   			  </button>
-  			  <span>{this.props.user}
-  			  </span>
   			</section>
     )
   }
